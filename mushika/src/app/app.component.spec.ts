ea0a6c16b967a26f5382b38ce8d1ed64c74f1d13
tjmukurumbira <tjmukurumbira@gmail.com>
2019-02-27 19:34:05 +0200

    mushika

--- mushika/src/app/app.component.spec.ts
+++ mushika/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mushikashika'`, () => {
+  it(`should have as title 'mushika'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('mushikashika');
+    expect(app.title).toEqual('mushika');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mushikashika!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mushika!');
   });
 });
